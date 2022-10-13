9f2e744a7dd30426f7fc4dd5c603e6c05e21dde9
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2022-10-13 23:17:43 +0300

    Add reflections

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -1,7 +1,31 @@
 package lotr;
 import org.reflections.Reflections;
+import static org.reflections.scanners.Scanners.SubTypes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.Set;
+
 public class CharacterFactory {
-    public void createCharacter() {
-        Reflections reflections = new Reflections();
+    public Character createCharacter() {
+        Reflections reflections = new Reflections("lotr");
+        Set<Class<?>> characters = reflections.get(SubTypes.of(Character.class).asClass());
+        characters.remove(Noble.class);
+        int index = new Random().nextInt(characters.size());
+        try {
+            // getting random out of Character possible options
+            return (Character) characters.toArray(new Class[0])[index].getDeclaredConstructor().newInstance();
+        }
+        // handling exceptions provided by IntelliJ
+        catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
